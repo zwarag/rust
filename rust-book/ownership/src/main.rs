169b2c9a3d752b1f2df86e5cad566d2345590e7a
Harrys Kavan <harrys.kavan@pm.com>
2022-08-01 12:05:35 +0200

    rustbook: ownership

---
+++ rust-book/ownership/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    let _s1 = gives_ownership();         // gives_ownership moves its return
+                                        // value into s1
+
+    let s2 = String::from("hello");     // s2 comes into scope
+
+    let _s3 = takes_and_gives_back(s2);  // s2 is moved into
+                                        // takes_and_gives_back, which also
+                                        // moves its return value into s3
+    println!("{}", s2); // s2 has been `drop` by `takes_and_gives_back(s2);`
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {             // gives_ownership will move its
+                                             // return value into the function
+                                             // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string                              // some_string is returned and
+                                             // moves out to the calling
+                                             // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into
+                                                      // scope
+
+    a_string  // a_string is returned and moves out to the calling function
+}
+
+fn ex1() {
+    let s1 = String::from("hello");
+    let _s2 = s1;
+
+    println!("{}, world!", s1);
+}
