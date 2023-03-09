0db54aef9d484083689942b69ce114b7bb05b8e8
zwarag <harrys.kavan@protonmail.com>
2023-03-09 07:17:34 +0100

    diff: refactor

--- diff/src/main.rs
+++ diff/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{fs::File, io::{BufRead}};
 
 fn synopsis() -> ! {
     panic!("Usage: diff path1 path2");
@@ -7,96 +7,62 @@ fn synopsis() -> ! {
 fn main() {
     let path1 = std::env::args().nth(1);
     let path2 = std::env::args().nth(2);
-    let file1: File;
-    let file2: File;
-    let mut buf_read1: std::io::Lines<BufReader<File>>;
-    let mut buf_read2: std::io::Lines<BufReader<File>>;
 
-    match path1 {
-        Some(p) => {
-            let _file1 = File::open(p);
-            match _file1 {
-                Ok(f) => file1 = f,
-                Err(err) => panic!("The first file does not exist!\n{:?}", err)
-            }
-        },
+    let file1 = match path1 {
+        Some(p) => File::open(p).expect("The first file does not exist!"),
         None => synopsis()
-    }
-    match path2 {
-        Some(p) => {
-            let _file2 = File::open(p);
-            match _file2 {
-                Ok(f) => file2 = f,
-                Err(err) => panic!("The second file does not exist!\n{:?}", err)
-            }
-        }
+    };
+    let file2 = match path2 {
+        Some(p) => File::open(p).expect("The second file does not exist!"),
         None => synopsis()
-    }
+    };
 
-    buf_read1 = std::io::BufReader::new(file1).lines();
-    buf_read2 = std::io::BufReader::new(file2).lines();
-
-    let mut diff = String::from("");
+    let mut buf_read1 = std::io::BufReader::new(file1).lines();
+    let mut buf_read2 = std::io::BufReader::new(file2).lines();
 
     let mut nextline1 = buf_read1.next();
     let mut nextline2 = buf_read2.next();
+
+    let mut diff = String::from("");
     
     loop {
         match (nextline1, nextline2) {
-            (Some(line1), Some(line2)) => {
-                match (line1, line2) {
-                    (Ok(line1), Ok(line2)) => {
-                        let mut chars1 = line1.chars();
-                        let mut chars2 = line2.chars();
-                        let mut char1 = chars1.next();
-                        let mut char2 = chars2.next();
-                        loop {
-                            match (char1, char2) {
-                                (Some(char1), Some(char2)) => {
-                                    if char1 != char2 {
-                                        diff.push(char2);
-                                    }
-                                },
-                                (Some(char1), None) => {
-                                    diff.push(char1);
-                                },
-                                (None, Some(char2)) => {
-                                    diff.push(char2)
-                                }, 
-                                (None, None) => {
-                                    // no more chars left in either file
-                                    break;
-                                }
+            (Some(Ok(line1)), Some(Ok(line2))) => {
+                let mut chars1 = line1.chars();
+                let mut chars2 = line2.chars();
+                let mut char1 = chars1.next();
+                let mut char2 = chars2.next();
+                loop {
+                    match (char1, char2) {
+                        (Some(char1), Some(char2)) => {
+                            if char1 != char2 {
+                                diff.push(char2);
                             }
-                            char1 = chars1.next();
-                            char2 = chars2.next();
+                        },
+                        (Some(char1), None) => {
+                            diff.push(char1);
+                        },
+                        (None, Some(char2)) => {
+                            diff.push(char2)
+                        }, 
+                        (None, None) => {
+                            // no more chars left in either file
+                            break;
                         }
-                    },
-                    _ => {
-                        panic!("oh no");
                     }
+                    char1 = chars1.next();
+                    char2 = chars2.next();
                 }
             },
             (Some(line1), None) => {
-                match line1 {
-                    Ok(line1) => {
-                        diff.push_str(&line1);
-                    },
-                    _ => {
-                        panic!("oh no");
-                    }
-                }
+                diff.push_str(&line1.unwrap());
             },
             (None, Some(line2)) => {
-                match line2 {
-                    Ok(line2) => {
-                        diff.push_str(&line2);
-                    },
-                    _ => {
-                        panic!("oh no");
-                    }
-                }
+                diff.push_str(&line2.unwrap());
             },
+            (Some(_), Some(_)) => {
+                panic!("oh no");
+            }
             (None, None) => {
                 // no more lines left in either file
                 break;
